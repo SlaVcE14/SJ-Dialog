970aeb18f46fe384de924d87b16cb47a34130228
SlaVcE <102253539+SlaVcE14@users.noreply.github.com>
2023-02-28 22:39:33 +0100

    Rename to BasicDialog

--- SJDialog/src/main/java/com/sjapps/library/customdialog/SetupDialog.java
+++ SJDialog/src/main/java/com/sjapps/library/customdialog/SetupDialog.java
@@ -3,6 +3,7 @@ package com.sjapps.library.customdialog;
  * by SlaVcE
  * Setting up custom dialog.
  * @deprecated SetupDialog is renamed to BasicDialog. Use {@link BasicDialog} instead.
+ * @since 1.6
  */
 @Deprecated
 public class SetupDialog extends BasicDialog{ }
