fc1852778fc40b29ad94b19dda42e558c99d28de
SlaVcE <102253539+SlaVcE14@users.noreply.github.com>
2024-02-25 01:30:09 +0100

    update javaDocs

--- SJDialog/src/main/java/com/sjapps/library/customdialog/DialogPreset.java
+++ SJDialog/src/main/java/com/sjapps/library/customdialog/DialogPreset.java
@@ -7,11 +7,13 @@ package com.sjapps.library.customdialog;
  * @see MessageDialog
  * @see ListDialog
  * @see CustomViewDialog
+ * @since 1.7
  * */
 public interface DialogPreset<D extends SJDialog> {
     /**
      * Add presets for the dialog
      * @param dialog presets will be applied to that dialog
+     * @since 1.7
      * */
     void addPresets(D dialog);
 }
